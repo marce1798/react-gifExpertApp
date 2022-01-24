87365a58f51deb0ee5dced38a19763bd96c8aa29
Marcelo Vivas <vivasmarcelo393@gmail.com>
2022-01-24 11:52:54 -0600

    index.html actualizado

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -10,7 +10,7 @@ export const GifGrid = ({category}) => {
         <>
             <h3 className='animate__animated animate__pulse'>{category}</h3>
             
-            {loading && <p className='animate__animated animate__flash'>Loading</p>}
+            {loading && <p className='animate__animated animate__flashrma'>Loading</p>}
 
             <div className='card-grid'>
             {
